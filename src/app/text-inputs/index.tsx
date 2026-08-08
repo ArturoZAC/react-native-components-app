f942b2bc4dc4e49a1059e373a4f34a464ce45556
ArturoZAC <arturoyz2105@gmail.com>
2026-08-08 18:39:52 -0500

    feat: implement TextInputsScreen with themed text input and form state management

--- src/app/text-inputs/index.tsx
+++ src/app/text-inputs/index.tsx
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native';
+import { useState } from "react";
+
+import ThemedCard from "@/src/components/shared/ThemedCard";
+import ThemedText from "@/src/components/shared/ThemedText";
+import ThemedTextInput from "@/src/components/shared/ThemedTextInput";
+import ThemedView from "@/src/components/shared/ThemedView";
 
 const TextInputsScreen = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    phoe: "",
+  });
+
   return (
-    <View>
-      <Text>TextInputsScreen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedCard className="mb-5">
+        <ThemedTextInput
+          placeholder="Nombre Completo"
+          autoCapitalize="words"
+          autoCorrect={false}
+          onChangeText={(text) => setForm({ ...form, name: text })}
+        />
+      </ThemedCard>
+
+      <ThemedCard>
+        <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+      </ThemedCard>
+    </ThemedView>
   );
 };
 export default TextInputsScreen;
