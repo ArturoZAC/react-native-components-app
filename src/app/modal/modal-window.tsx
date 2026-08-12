6339195203142561f0a5fa2238b9bd87253d2b89
ArturoZAC <arturoyz2105@gmail.com>
2026-08-11 19:37:46 -0500

    feat: add StatusBar handling to ModalWindowScreen for improved appearance

--- src/app/modal/modal-window.tsx
+++ src/app/modal/modal-window.tsx
@@ -1,3 +1,7 @@
+import { Platform } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
+
 import ThemedText from "@/src/components/shared/ThemedText";
 import ThemedView from "@/src/components/shared/ThemedView";
 
@@ -5,6 +9,8 @@ const ModalWindowScreen = () => {
   return (
     <ThemedView className="flex-1 items-center justify-center" bgColor="#A52182">
       <ThemedText>Hola, soy un modal</ThemedText>
+
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </ThemedView>
   );
 };
